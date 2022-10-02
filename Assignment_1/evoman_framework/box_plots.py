891e30ff7e99e4658ebd9ec2da4be9c08c7a6709
Jordan Phillips <jordan.m.phillips23@gmail.com>
2022-10-02 23:50:06 +0200

    added box plot code

---
+++ Assignment_1/evoman_framework/box_plots.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("best_results_7.csv")
+
+df["Enemy 7 Best"] = df["best"]
+boxplot = df.boxplot(column="Enemy 7 Best")
+
+boxplot.set_ylabel("Fitness")
+boxplot.set_title("Enemy 7 Best Fitness Box Plot")
+plt.show()
