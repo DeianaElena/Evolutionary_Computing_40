13e6c35a6258caf37c4bbf973e0e8dd0113f1fd5
Elena <75449340+DeianaElena@users.noreply.github.com>
2022-10-03 17:27:14 +0200

    added boxplot label

--- Assignment_1/evoman_framework/box_plots.py
+++ Assignment_1/evoman_framework/box_plots.py
@@ -1,8 +1,13 @@
+#%%
 import pandas as pd
+import os
 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("best_results_7git.csv")
+cwd = os.getcwd()
+print('current wd', cwd)
+
+df = pd.read_csv("results_best_selection/best_results_7.csv")
 
 df["Enemy 7 Best"] = df["best"]
 boxplot = df.boxplot(column="Enemy 7 Best")
@@ -10,3 +15,5 @@ boxplot = df.boxplot(column="Enemy 7 Best")
 boxplot.set_ylabel("Fitness")
 boxplot.set_title("Enemy 7 Best Fitness Box Plot")
 plt.show()
+
+# %%
