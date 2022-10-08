acf3100db23e0317e746dc7efc3e583ecc4c7674
Elena <75449340+DeianaElena@users.noreply.github.com>
2022-10-08 23:53:30 +0200

    multiplemode and runtime tracking

--- Assignment_2/40/group_40_best.py
+++ Assignment_2/40/group_40_best.py
@@ -5,12 +5,15 @@ import csv
 import sys, os
 import pandas as pd
 from group_40_controller import group40Controller
+import time
 
+in_seconds = time.time()  # added sets time marker
+in_local_time = time.ctime(in_seconds)  # added local time
 
 sys.path.insert(0, 'evoman')
 from environment import Environment
 
-experiment_name = 'group_40c'
+experiment_name = 'group_40_Best'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
@@ -28,7 +31,23 @@ OUTPUTS=5
 
 POP_SIZE=3
 
-ENEMY=5
+N_GEN = 20
+
+#testing enemies:
+g1 = [1,5,7]            #each with different action number
+g2 = [2,6,8]            #each with different action number
+g3 = [5,6]              #same action number of 3
+g4 = [1,2]              #same action number of 4
+g5 = [7,8]              #same action number of 6
+g6 = [1,5]              #actionn 4 and action 3
+g7 = [2,5]              #actionn 4 and action 3
+g8 = [6,7]              #action 3 and action 6
+g9 = [5,8]              #action 3 and action 6
+g10 = [4,6]             #action 4 and action 3
+g11 = [3,5]             #action 4 and action 3
+
+
+ENEMY=g5    #manually change this
 
 total_weights = (INPUTS + 1) * NEURONS + (NEURONS + 1) * OUTPUTS
 
@@ -47,12 +66,13 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evaluate(ind):
     env = Environment(
         experiment_name=experiment_name,
-        enemies=[ENEMY],
+        enemies=ENEMY,
         level=2,
         playermode="ai",
         player_controller=group40Controller(ind),
         enemymode="static",
-        speed="fastest"
+        speed="fastest",
+        multiplemode="yes"
     )
     env.play()
     return (env.fitness_single(),)
@@ -79,7 +99,7 @@ for i in range(1, 11):
     pop = toolbox.population(n=POP_SIZE)
 
     pop, logbook = algorithms.eaMuCommaLambda(pop, toolbox, mu=POP_SIZE, lambda_=7, halloffame=hof,
-                cxpb=0.4, mutpb=0.5, ngen=30, stats=stats, verbose=True)
+                cxpb=0.4, mutpb=0.5, ngen=N_GEN, stats=stats, verbose=True)
     tot = 0
     for j in range(5):
         for ind in hof:
@@ -95,3 +115,24 @@ for i in range(1, 11):
 best_log = pd.DataFrame()
 best_log["best"] = best_runs
 best_log.to_csv(f'results_best_selection/best_results_{ENEMY}.csv', index=False)
+
+
+#### experiment runtime
+end_seconds = time.time()       #print total execution time for experiment
+end_local_time = time.ctime(end_seconds)
+ex_time = str(round((end_seconds-in_seconds)/60))
+print( '\nExecution time: '+ ex_time +' minutes \n')
+
+#add info of experiment in a csv file with some details
+run_data = {'Ending local time' : end_local_time,
+            'Runtime:' : ex_time,
+            'N_generations:' : N_GEN,
+            'Pop_size:' : POP_SIZE,
+            'Enemy:': [ENEMY]
+            }
+
+df_runtime= pd.DataFrame(run_data)
+df_runtime.to_csv(f'results_best_selection/runtime.csv', sep = '|', mode='a', index=False, header=False)   #appending data every time we run an experiment to keep track of enemies and runtime
+
+
+
