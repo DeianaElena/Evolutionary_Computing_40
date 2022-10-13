62ff810e946b6402e713fe5d149d7b0e59e73833
Lakoeke <jochem@homus.nl>
2022-10-13 20:18:35 +0200

    updated box and bestSel

--- Assignment_2/40/box_plots.py
+++ Assignment_2/40/box_plots.py
@@ -1,19 +1,43 @@
 #%%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 import pandas as pd
 import os
 
-import matplotlib.pyplot as plt
+columns = ["gen", "nevals", "avg", "std", "min", "max"]
 
-cwd = os.getcwd()
-print('current wd', cwd)
+ENEMY=[1,5,7]
+f'results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}.csv'
+df_product = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}1.csv", usecols=columns, index_col=None)
+df_product1 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}2.csv", usecols=columns, index_col=None)
+df_product2 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}3.csv", usecols=columns, index_col=None)
+df_product3 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}4.csv", usecols=columns, index_col=None)
+df_product4 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}5.csv", usecols=columns, index_col=None)
+df_product5 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}6.csv", usecols=columns, index_col=None)
+df_product6 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}7.csv", usecols=columns, index_col=None)
+df_product7 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}8.csv", usecols=columns, index_col=None)
+df_product8 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}9.csv", usecols=columns, index_col=None)
+df_product9 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}10.csv", usecols=columns, index_col=None)
 
-df = pd.read_csv("results_best_selection/best_results_7.csv")
+df_product_final = (pd.concat([df_product, df_product1, df_product2, df_product3, df_product4, df_product5, df_product6, df_product7, df_product8, df_product9]).groupby('gen', as_index=False).
+                      agg({"avg": "mean", "std": "mean", "max": "mean", "min": "mean"}))
 
-df["Enemy 7 Best"] = df["best"]
-boxplot = df.boxplot(column="Enemy 7 Best")
+df_product_final_med = (pd.concat([df_product, df_product1, df_product2, df_product3, df_product4, df_product5, df_product6, df_product7, df_product8, df_product9]).groupby('gen', as_index=False).
+                      agg({"avg": "median"}))
 
-boxplot.set_ylabel("Fitness")
-boxplot.set_title("Enemy 7 Best Fitness Box Plot")
+df_product_final["std1"] = df_product_final["std"]
+df_product_final["max1"] = df_product_final["max"] #changed this because pandas reads "max" as a command
+
+df_product_final_med["avg_median"] = df_product_final_med.std(axis=1)
+
+data = np.concatenate((df_product_final['avg'], df_product_final_med["avg_median"], df_product_final["max"], df_product_final["min"]))
+
+fig1, ax1 = plt.subplots()
+ax1.set(ylabel='AVERAGE FITNESS',
+       title=f'BEST SELECTION ALGORITHM - ENEMY {ENEMY}')
+ax1.boxplot(data)
+
 plt.show()
 
 # %%
--- Assignment_2/40/plots_multi_bestSel copy.py
+++ Assignment_2/40/plots_multi_bestSel copy.py
@@ -16,17 +16,17 @@ import os
 columns = ["gen", "nevals", "avg", "std", "min", "max"]
 #define enemy
 ENEMY=[1,5,7]
-f'results_best_selection/best_results_{ENEMY}.csv'
-df_product = pd.read_csv(f"results_best_selection/test{ENEMY}1.csv", usecols=columns, index_col=None)
-df_product1 = pd.read_csv(f"results_best_selection/test{ENEMY}2.csv", usecols=columns, index_col=None)
-df_product2 = pd.read_csv(f"results_best_selection/test{ENEMY}3.csv", usecols=columns, index_col=None)
-df_product3 = pd.read_csv(f"results_best_selection/test{ENEMY}4.csv", usecols=columns, index_col=None)
-df_product4 = pd.read_csv(f"results_best_selection/test{ENEMY}5.csv", usecols=columns, index_col=None)
-df_product5 = pd.read_csv(f"results_best_selection/test{ENEMY}6.csv", usecols=columns, index_col=None)
-df_product6 = pd.read_csv(f"results_best_selection/test{ENEMY}7.csv", usecols=columns, index_col=None)
-df_product7 = pd.read_csv(f"results_best_selection/test{ENEMY}8.csv", usecols=columns, index_col=None)
-df_product8 = pd.read_csv(f"results_best_selection/test{ENEMY}9.csv", usecols=columns, index_col=None)
-df_product9 = pd.read_csv(f"results_best_selection/test{ENEMY}10.csv", usecols=columns, index_col=None)
+f'results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}.csv'
+df_product = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}1.csv", usecols=columns, index_col=None)
+df_product1 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}2.csv", usecols=columns, index_col=None)
+df_product2 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}3.csv", usecols=columns, index_col=None)
+df_product3 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}4.csv", usecols=columns, index_col=None)
+df_product4 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}5.csv", usecols=columns, index_col=None)
+df_product5 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}6.csv", usecols=columns, index_col=None)
+df_product6 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}7.csv", usecols=columns, index_col=None)
+df_product7 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}8.csv", usecols=columns, index_col=None)
+df_product8 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}9.csv", usecols=columns, index_col=None)
+df_product9 = pd.read_csv(f"results_best_selection/Elena_tests/pop50_lam80_gen20_e157_t149/test{ENEMY}10.csv", usecols=columns, index_col=None)
 
 
 df_product_final = (pd.concat([df_product, df_product1, df_product2, df_product3, df_product4, df_product5, df_product6, df_product7, df_product8, df_product9]).groupby('gen', as_index=False).
