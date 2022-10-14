2e1ec601e7f07c74e864cc4985968fb52363adb6
Nikita <tiwarinikita98@gmail.com>
2022-10-14 20:01:54 +0200

    adds tournament results pop 50, lamda 65, gen 15

--- Assignment_2/40/group_40_tournament.py
+++ Assignment_2/40/group_40_tournament.py
@@ -9,7 +9,6 @@ from group_40_controller import group40Controller
 in_seconds = time.time()  # added sets time marker
 in_local_time = time.ctime(in_seconds)  # added local time
 
-
 sys.path.insert(0, 'evoman')
 from environment import Environment
 
@@ -23,34 +22,36 @@ creator.create("FitnessEvo", base.Fitness, weights=(1.0,))
 creator.create("Individual", np.ndarray, fitness=creator.FitnessEvo, strategy=None)
 creator.create("Strategy", np.ndarray)
 
-#set parameters
-NEURONS=10
+# set parameters
+NEURONS = 10
 
-INPUTS=20
+INPUTS = 20
 
-OUTPUTS=5
+OUTPUTS = 5
 
-#editable parameters
+# editable parameters
 
-POP_SIZE=5
-LAMBDA = 35
+POP_SIZE = 50
+LAMBDA = 65
 
-N_GEN = 20
+N_GEN = 15
 
-#testing enemies:
-g1 = [1,5,7]            #each with different action number
-g2 = [2,6,8]            #each with different action number
-g3 = [5,6]              #same action number of 3
-g4 = [1,2]              #same action number of 4
-g5 = [7,8]              #same action number of 6
-g6 = [1,5]              #actionn 4 and action 3
-g7 = [2,5]              #actionn 4 and action 3
-g8 = [6,7]              #action 3 and action 6
-g9 = [5,8]              #action 3 and action 6
-g10 = [4,6]             #action 4 and action 3
-g11 = [3,5]             #action 4 and action 3
+# testing enemies:
+g1 = [1, 5, 7]  # each with different action number
+g2 = [2, 6, 8]  # each with different action number
+g3 = [5, 6]  # same action number of 3
+g4 = [1, 2]  # same action number of 4
+g5 = [7, 8]  # same action number of 6
+g6 = [1, 5]  # action 4 and action 3
+g7 = [2, 5]  # action 4 and action 3
+g8 = [6, 7]  # action 3 and action 6
+g9 = [5, 8]  # action 3 and action 6
+g10 = [4, 6]  # action 4 and action 3
+g11 = [3, 5]  # action 4 and action 3
+g12 = [3,4,6]           #two of action 4 and one action 3
+g13=[1,2,3,4,5,6,7,8]   #all enemies
 
-ENEMY=g1         #manually change this
+ENEMY = g8  # manually change this
 
 total_weights = (INPUTS + 1) * NEURONS + (NEURONS + 1) * OUTPUTS
 
@@ -60,11 +61,13 @@ def genInd(ind_cls, strat_cls, total_weights, scale):
     ind.strategy = strat_cls(np.random.random((total_weights)))
     return ind
 
+
 toolbox = base.Toolbox()
 
 toolbox.register("individual", genInd, creator.Individual, creator.Strategy, total_weights, 20)
 
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
 
 def evaluate(ind):
     env = Environment(
@@ -79,6 +82,7 @@ def evaluate(ind):
     )
     results = env.play()
     return (results[0],)
+
 
 def final_evaluate(ind):
     out_arr = []
@@ -103,7 +107,7 @@ toolbox.register("evaluate", evaluate)
 
 toolbox.register("mate", tools.cxESBlend, alpha=0.1)
 toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.5)
-toolbox.register("select", tools.selTournament, tournsize=4) #tournament selection
+toolbox.register("select", tools.selTournament, tournsize=4)  # tournament selection
 
 stats = tools.Statistics(lambda ind: ind.fitness.values)
 stats.register("avg", np.mean)
@@ -123,8 +127,7 @@ for i in range(1, 11):
     pop = toolbox.population(n=POP_SIZE)
 
     pop, logbook = algorithms.eaMuPlusLambda(pop, toolbox, mu=POP_SIZE, lambda_=LAMBDA, halloffame=hof,
-                cxpb=0.4, mutpb=0.5, ngen=N_GEN, stats=stats, verbose=True)
-
+                                             cxpb=0.4, mutpb=0.5, ngen=N_GEN, stats=stats, verbose=True)
 
     for ind in hof:
         current_ind = ind
@@ -139,7 +142,6 @@ for i in range(1, 11):
     print("BEST", j, tot)
     best_runs.append(tot)
 
-
     df_log = pd.DataFrame(logbook)
     df_log.to_csv(f'results_tournament_selection/test{ENEMY}{i}.csv', index=False)
 
@@ -153,21 +155,20 @@ out_df.to_csv(f'results_tournament_selection/test{ENEMY}_final_results.csv', ind
 
 np.savetxt(f'results_tournament_selection/test{ENEMY}_best_weights.txt', best_ind)
 
-
-
 #### experiment runtime
-end_seconds = time.time()       #print total execution time for experiment
+end_seconds = time.time()  # print total execution time for experiment
 end_local_time = time.ctime(end_seconds)
-ex_time = str(round((end_seconds-in_seconds)/60))
-print( '\nExecution time: '+ ex_time +' minutes \n')
+ex_time = str(round((end_seconds - in_seconds) / 60))
+print('\nExecution time: ' + ex_time + ' minutes \n')
 
-#add info of experiment in a csv file with some details
-run_data = {'Ending local time' : end_local_time,
-            'Runtime:' : ex_time,
-            'N_generations:' : N_GEN,
-            'Pop_size:' : POP_SIZE,
+# add info of experiment in a csv file with some details
+run_data = {'Ending local time': end_local_time,
+            'Runtime:': ex_time,
+            'N_generations:': N_GEN,
+            'Pop_size:': POP_SIZE,
             'Enemy:': [ENEMY]
             }
 
-df_runtime= pd.DataFrame(run_data)
-df_runtime.to_csv(f'results_tournament_selection/runtime_t.csv', sep = '|', mode='a', index=False, header=False)   #appending data every time we run an experiment to keep track of enemies and runtime
+df_runtime = pd.DataFrame(run_data)
+df_runtime.to_csv(f'results_tournament_selection/runtime_t.csv', sep='|', mode='a', index=False,
+                  header=False)  # appending data every time we run an experiment to keep track of enemies and runtime
